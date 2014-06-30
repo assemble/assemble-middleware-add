94031fd57a2a4b29780cb4bde980caf2915621c6
Brian Woodward <brian.woodward@gmail.com>
2014-06-30 11:53:30 -0400

    initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-middleware-add <https://github.com/assemble/assemble-middleware-add>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
